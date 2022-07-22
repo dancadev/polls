fb6cde2491c39b06cfb51acab4cc3c0261850ef4
dancadev <62481416+dancadev@users.noreply.github.com>
2022-07-22 17:34:18 -0500

    clave

--- polls/polls/settings.py
+++ polls/polls/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zbm!uc@sl!ov45ebcr*tftcor)!h$0+)4#9f$z8nzr)q82*!zr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
